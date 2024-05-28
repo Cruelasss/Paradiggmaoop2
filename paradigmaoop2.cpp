63deaff3703df5ac103f7e4dc30e6db1b3f4e5c1
Cruelas <irula5198@gmail.com>
2024-05-28 15:14:12 +0700

    commit2

--- paradigmaoop2.cpp
+++ paradigmaoop2.cpp
@@ -12,4 +12,12 @@ class orang {
         cout << "Orang dibuat dengan umur" << umur << "\n" << endl;
     }
 };
+class pekerja : public orang{
+    public: 
+    pekerja (int pUmur) :
+    orang (pUmur) 
+    {
+        cout << "Pekerja dibuat\n" << endl;
+    }
+};
 
