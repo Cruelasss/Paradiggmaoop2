860e4ec005260a518c7f156e5eb7887e3d6b5949
Cruelas <irula5198@gmail.com>
2024-05-28 15:15:04 +0700

    commit4

--- paradigmaoop2.cpp
+++ paradigmaoop2.cpp
@@ -29,3 +29,19 @@ class pelajar : public orang {
     }
 
 };
+class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+	{
+		cout << "Budi dibuat\n" << endl;
+	}
+};
+
+
+
+
+
+
