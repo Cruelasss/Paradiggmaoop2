66bee1b53d4908cbf2d4ba6e462c1d251e24c62f
Cruelas <irula5198@gmail.com>
2024-05-28 15:11:04 +0700

    first commit

---
+++ polimorphy.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+public:
+	virtual void pesan() = 0;
+	//virtual void pesan() {
+		//cout << "Pesan dari seseorang" << endl;
+	//}
+	//virtual void cetak() = 0;
+	virtual void cetak() {
+
+	}
+};
+
+class joko : public seseorang {
+public:
+	void pesan() {
+		cout << "Pesan dari Joko" << endl;
+	}
+	void cetak() {
+		cout << "Tulisan Joko" << endl;
+	}
+};
+
+class lia : public seseorang {
+	void pesan() {
+		cout << "Pesan dari Lia" << endl;
+	}
+
+
+};
+
+int main() {
+	seseorang* orang;
+	joko a;
+	lia b;
+
+	orang = &a;
+	orang->pesan();
+	orang = &b;
+	orang->pesan();
+	//a.seseorang::pesan();	//Memanggil method yang berada di class orang tua
+	//b.seseorang::pesan();
+};
