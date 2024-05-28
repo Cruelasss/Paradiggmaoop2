68a8cc57490817b35cbf9036bcd0d110193dfe5e
Cruelas <irula5198@gmail.com>
2024-05-28 15:43:16 +0700

    commit20

--- Enkaptulasi.cpp
+++ Enkaptulasi.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
+class remoteLampu {
+private:
+    string saklarNo[10];
+public:
+	void SetSaklarNo(int i, string value) {
+		saklarNo[i] = value;
+	}
+	string GetSaklarNo(int i) {
+		return saklarNo[i];
+	}
+};
 
